3efb380dd823ecec98851b31f5be1cd182468223
sidharthpanicker <sidharthpanicker@rocketmail.com>
2019-03-24 19:34:41 -0700

    AutoScaler Updated by Namitha

--- WebApp/src/main/java/com/example/demo/AutoScaler.java
+++ WebApp/src/main/java/com/example/demo/AutoScaler.java
@@ -40,61 +40,117 @@ public class AutoScaler {
 		 return freeRunningEc2Ids;
 	 }
 	 
-	 public static void ScaleInOut()
-	 {
-		
-		List<String> runningEC2Ids = getIdsOfRunningInstances();
-		List<String> stoppedEC2Ids = getIdsOfStoppedInstances();
-		int numOfAppEC2 = runningEC2Ids.size() -1;
-		List<String> freeRunningEc2Ids = getIdsOfFreeRunningInstances();
-		int capacity = freeRunningEc2Ids.size() + stoppedEC2Ids.size();
-		System.out.println("Number of free running ec2 "+ freeRunningEc2Ids.size() +"\n");
-		
-		// Query the queue for number of messages
-		AmazonS3 s3client = getS3Client();
-		int numberOfMsgs = getApproximateNumberOfMsgs();
-		System.out.println("Number of messages "+ numberOfMsgs +"\n");
-					
-		
-		// Scale Out
-		if (numberOfMsgs > 0 && numberOfMsgs > capacity && numOfAppEC2 < MAXIMUM_NO_OF_INSTANCES )
-		{
-			int num = numberOfMsgs - capacity;
-			if (num > MAXIMUM_NO_OF_INSTANCES )
-			{
-				num = MAXIMUM_NO_OF_INSTANCES;
-			}
-			System.out.println("Scale out");
-    		int status = CreateInstance(IMAGE_ID, num);
-    		for (int i=0;i<stoppedEC2Ids.size();i++)
-    		{
-    			startInstance(stoppedEC2Ids.get(i));
-    		}
-			
-		}
-		//Scale In
-		if (numOfAppEC2 > MINIMUM_NO_OF_INSTANCES && numberOfMsgs < capacity)
-		{
-			int num = numOfAppEC2 - numberOfMsgs;
-			if (numberOfMsgs < MINIMUM_NO_OF_INSTANCES)
-			{
-				num = num - MINIMUM_NO_OF_INSTANCES;
-			}
-			System.out.println("Scale in");
-			for (int i=0;i<num;i++)
-			{
-				stopInstance(freeRunningEc2Ids.get(i));
-			}
-		}
-		int i = 0;
-		runningEC2Ids = getIdsOfFreeRunningInstances();
-		while(i< runningEC2Ids.size())
-		{
-			Message message = receiveMessage();
-			s3client.putObject(BUCKET_NAME, runningEC2Ids.get(i),message.getBody());
-			deleteMessage(message);
-			i++;
+	 public static void ScaleInOut() {
 
+		 List<String> runningEC2Ids = getIdsOfRunningInstances();
+		 List<String> stoppedEC2Ids = getIdsOfStoppedInstances();
+		 int numOfAppEC2 = runningEC2Ids.size() - 1;
+		 List<String> freeRunningEc2Ids = getIdsOfFreeRunningInstances();
+		 int capacity = freeRunningEc2Ids.size() + stoppedEC2Ids.size();
+
+		 System.out.println("Number of running ec2 " + runningEC2Ids.size() + "\n");
+		 System.out.println("Number of free running ec2 " + freeRunningEc2Ids.size() + "\n");
+		 System.out.println("Number of stopped ec2 " + stoppedEC2Ids.size() + "\n");
+
+		 // Query the queue for number of messages
+		 AmazonS3 s3client = getS3Client();
+		 int numberOfMsgs = getApproximateNumberOfMsgs();
+		 System.out.println("Number of messages " + numberOfMsgs + "\n");
+
+
+/*		 // Scale Out
+		 if (numberOfMsgs > 0 && numberOfMsgs > freeRunningEc2Ids.size() && numOfAppEC2 < MAXIMUM_NO_OF_INSTANCES) {
+		 	if (numberOfMsgs < stoppedEC2Ids.size())
+			 int num = numberOfMsgs - capacity;
+			 if (num > MAXIMUM_NO_OF_INSTANCES) {
+				 num = MAXIMUM_NO_OF_INSTANCES;
+			 }
+			 System.out.println("Scale out");
+			 int status = CreateInstance(IMAGE_ID, num);
+			 for (int i = 0; i < stoppedEC2Ids.size(); i++) {
+				 startInstance(stoppedEC2Ids.get(i));
+			 }
+
+		 }
+		 if( numberOfMsgs < capacity) {
+			 if(numberOfMsgs > 0 && numberOfMsgs <= stoppedEC2Ids.size() && numberOfMsgs> freeRunningEc2Ids.size() ) {
+				int num =  freeRunningEc2Ids.size() - numberOfMsgs;
+			 	for (int i=0;i<num;i++)
+				 {
+					 startInstance(stoppedEC2Ids.get(i));
+				 }
+			 }
+			 */
+
+			// Scale Out
+		 	if(numberOfMsgs> 0 && numberOfMsgs> capacity)
+			{
+				if(numberOfMsgs > freeRunningEc2Ids.size())
+				{
+					int num = numberOfMsgs - capacity;
+					if (num > MAXIMUM_NO_OF_INSTANCES) {
+						num = MAXIMUM_NO_OF_INSTANCES;
+					}
+					System.out.println("Scale out");
+					System.out.println("Creating new Instances");
+					int status = CreateInstance(IMAGE_ID, num);
+					for (int i = 0; i < stoppedEC2Ids.size(); i++) {
+						System.out.println("Start Stopped instances");
+						startInstance(stoppedEC2Ids.get(i));
+					}
+				}
+
+			}
+		 	else if(numberOfMsgs< capacity)
+			{
+				if(numberOfMsgs > freeRunningEc2Ids.size() && (numberOfMsgs-freeRunningEc2Ids.size())<=stoppedEC2Ids.size())
+				{
+					int startinstancenum= (numberOfMsgs-freeRunningEc2Ids.size());
+					for (int i=0;i<startinstancenum;i++)
+					{
+						System.out.println("Start Stopped instances");
+						startInstance(stoppedEC2Ids.get(i));
+					}
+				}
+
+			}
+		 	// Scale In
+		 	if(numOfAppEC2 > MINIMUM_NO_OF_INSTANCES && numberOfMsgs < freeRunningEc2Ids.size()){
+			 int num = freeRunningEc2Ids.size() - numberOfMsgs;
+			 if (numberOfMsgs < MINIMUM_NO_OF_INSTANCES) {
+				 num = num - MINIMUM_NO_OF_INSTANCES;
+			 }
+			 System.out.println("Scale in");
+			 for (int i = 0; i < num; i++) {
+				 System.out.println("Stop instances");
+				 stopInstance(freeRunningEc2Ids.get(i));
+			 }
+		 }
+		if(numberOfMsgs > 0 && numberOfMsgs < capacity){
+
+			 for (int i=0;i<numberOfMsgs;i++)
+			 {
+				 System.out.println("Starting instances");
+				 startInstance(stoppedEC2Ids.get(i));
+			 }
+		 }
+		//Add key value (instanceID, Video name) in S3
+		if(numberOfMsgs > 0) {
+			int i = 0;
+			runningEC2Ids = getIdsOfFreeRunningInstances();
+			while (i < runningEC2Ids.size() && i< numberOfMsgs) {
+				Message message = receiveMessage();
+
+				if(message != null) {
+					String InstId = runningEC2Ids.get(i);
+					System.out.println("Adding message and video name to S3");
+					s3client.putObject(BUCKET_NAME, InstId , message.getBody());
+					System.out.println("Deleting message from sqs");
+					deleteMessage(message);
+				}
+				i++;
+
+			}
 		}
 	 }
 }
--- WebApp/src/main/java/com/example/demo/Configuration.java
+++ WebApp/src/main/java/com/example/demo/Configuration.java
@@ -3,13 +3,22 @@ package com.example.demo;
 import com.amazonaws.regions.Regions;
 
 public  class Configuration {
-    public static final String BUCKET_NAME = "cloud-sprng-19";
+
+
+
+    //Chandana
+    public static final String BUCKET_NAME = "cloud-iaas-project";
+    public static final String ACCESS_KEY = "AKIAI7LTFYPHDGK7NSUA";
+    public static final String SECRET_KEY = "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";
+
     public static final String RASBERRY_PIE_URL = "http://206.207.50.7/getvideo";
-    public static final String ACCESS_KEY = "AKIAIMDJVS6GVNISDHRA";
-    public static final String SECRET_KEY = "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";
+   /* public static final String ACCESS_KEY = "AKIAIMDJVS6GVNISDHRA";
+    public static final String BUCKET_NAME = "cloud-sprng-19";
+    public static final String SECRET_KEY = "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";*/
+
     public static final Regions REGION = Regions.US_WEST_1;
     public static final String QUEUE_NAME = "cloud-computing.fifo";
-    public static final int LOAD_BALANCER_TIMEOUT = 30;
+    public static final int LOAD_BALANCER_TIMEOUT = 20;
     public static final int WEB_APP_TIMEOUT = 5;
     public static final int MINIMUM_NO_OF_INSTANCES = 2;
     public static final int MAXIMUM_NO_OF_INSTANCES = 19;
