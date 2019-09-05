9931ccf0e11538d1ee5d4c776755f6e4ef4d9143
Sidharth Panicker <sidharthpanicker@rocketmail.com>
2019-09-04 21:45:43 -0700

    Update Configuration.java

--- WebApp/src/main/java/com/example/demo/Configuration.java
+++ WebApp/src/main/java/com/example/demo/Configuration.java
@@ -8,19 +8,10 @@ public  class Configuration {
 
     //Chandana
     public static final String BUCKET_NAME = "cloud-iaas-project";
-    public static final String ACCESS_KEY = "AKIAI7LTFYPHDGK7NSUA";
-    public static final String SECRET_KEY = "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";
+    public static final String ACCESS_KEY = "";
+    public static final String SECRET_KEY = "";
 
     public static final String RASBERRY_PIE_URL = "http://206.207.50.7/getvideo";
-
-//    public static final String ACCESS_KEY = "AKIAIMDJVS6GVNISDHRA";
-//    public static final String BUCKET_NAME = "cloud-sprng-19";
-//    public static final String SECRET_KEY = "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";
-
-/*    public static final String ACCESS_KEY = "AKIAIMDJVS6GVNISDHRA";
-    public static final String BUCKET_NAME = "cloud-sprng-19";
-    public static final String SECRET_KEY = "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";*/
-
 
     public static final Regions REGION = Regions.US_WEST_1;
     public static final String QUEUE_NAME = "cloud-computing.fifo";
